057f7c79bab359def7c8380d55926e643c23589d
Fishywishi <69370259+Fishywishi@users.noreply.github.com>
2023-04-17 13:20:57 -0500

    Delete src/main directory

--- src/main/java/Fishywishi/PGMAutoGG/Reference.java
+++
@@ -1,8 +0,0 @@
-package Fishywishi.PGMAutoGG;
-
-public class Reference {
-	public static final String MOD_ID = "pgmgg";
-	public static final String Mod_NAME = "PGM Auto GG";
-	public static final String Version = "1.0";
-
-}
